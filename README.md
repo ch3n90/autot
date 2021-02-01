d4b0e7e0d18013141e81f6e4f1d2ffe42c2f5985
ch3ng <chengzhang19@iflytek.com>
2021-02-01 16:31:20 +0800

    modified readme

--- README.md
+++ README.md
@@ -2,7 +2,68 @@
 autot 是一个自动任务容器
 
 ## 任务类型
-- 时间戳到点执行
+- 指定执行时间
 - 延迟执行
-- 自定义延迟执行。延迟执行多少次，每次执行间隔可调
 - 函数式执行
+
+## 如何使用
+- 指定执行时间
+```java
+
+    @Test
+    public void testTimestamp() throws IOException {
+        TaskContainer taskContainer = TaskContainer.INSTANCE();
+        taskContainer.put(new TimestampTimeType(System.currentTimeMillis() + 1000 * 20), new AbstractTask(){
+            @Override
+            public void job() {
+                System.out.println("20s, start running");
+            }
+        });
+
+        System.in.read();
+    }
+```
+
+- 延迟执行
+```java
+    @Test
+    public void testTTL() throws IOException {
+        TaskContainer taskContainer = TaskContainer.INSTANCE();
+        taskContainer.put(new TTLTimeType(30, TimeUnit.SECONDS), new AbstractTask() {
+            @Override
+            public void job() {
+                System.out.println("30s, start running");
+            }
+        });
+
+        System.in.read();
+    }
+```
+
+- 函数式执行
+```java
+    /**
+     * 自定义函数实现运行
+     */
+@Test
+    public void testFunc() throws IOException {
+        TaskContainer taskContainer = TaskContainer.INSTANCE();
+
+        taskContainer.put(new CustomizedFunctionTimeType<CustomizedFunctionParam>(new CustomizedFunctionParam(5,System.currentTimeMillis() + 5 * 1000),
+                rt -> {
+                    if(rt.timestamp < System.currentTimeMillis()){
+                        System.out.println("current index:" + rt.index);
+                        return new CustomizedFunctionResult(true,System.currentTimeMillis() + 1000 * 15);
+                    }else {
+                        return new CustomizedFunctionResult(false);
+                    }
+
+                }),new AbstractTask() {
+            @Override
+            public void job() {
+                System.out.println("start running");
+            }
+        });
+        System.in.read();
+    }
+```
--- src/test/java/com/milchstrabe/autot/AppTest.java
+++ src/test/java/com/milchstrabe/autot/AppTest.java
@@ -8,28 +8,48 @@ public class AppTest
 {
 
     @Test
-    public void test() throws IOException {
+    public void testTimestamp() throws IOException {
         TaskContainer taskContainer = TaskContainer.INSTANCE();
-//        taskContainer.put(new TTL(30, TimeUnit.SECONDS), new AbstractTask() {
-//            @Override
-//            public void job() {
-//                System.out.println("30s, start running");
-//            }
-//        });
-
-        taskContainer.put(new CustomizedFunctionTimeType<CustomizedFunctionParam>(new CustomizedFunctionParam(5,System.currentTimeMillis() + 5 * 1000),
-            a -> {
-                if(a.timestamp < System.currentTimeMillis()){
-                    System.out.println("current index:" + a.index);
-                    return new CustomizedFunctionResult(true,System.currentTimeMillis() + 1000 * 15);
-                }else {
-                    return new CustomizedFunctionResult(false);
-                }
-
-            }),new AbstractTask() {
+        taskContainer.put(new TimestampTimeType(System.currentTimeMillis() + 1000 * 20), new AbstractTask() {
             @Override
             public void job() {
-                System.out.println(" start running");
+                System.out.println("20s, start running");
+            }
+        });
+
+        System.in.read();
+    }
+
+    @Test
+    public void testTTL() throws IOException {
+        TaskContainer taskContainer = TaskContainer.INSTANCE();
+        taskContainer.put(new TTLTimeType(30, TimeUnit.SECONDS), new AbstractTask() {
+            @Override
+            public void job() {
+                System.out.println("30s, start running");
+            }
+        });
+
+        System.in.read();
+    }
+
+    @Test
+    public void testFunc() throws IOException {
+        TaskContainer taskContainer = TaskContainer.INSTANCE();
+
+        taskContainer.put(new CustomizedFunctionTimeType<CustomizedFunctionParam>(new CustomizedFunctionParam(5,System.currentTimeMillis() + 5 * 1000),
+                rt -> {
+                    if(rt.timestamp < System.currentTimeMillis()){
+                        System.out.println("current index:" + rt.index);
+                        return new CustomizedFunctionResult(true,System.currentTimeMillis() + 1000 * 15);
+                    }else {
+                        return new CustomizedFunctionResult(false);
+                    }
+
+                }),new AbstractTask() {
+            @Override
+            public void job() {
+                System.out.println("start running");
             }
         });
         System.in.read();
